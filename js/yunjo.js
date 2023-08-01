9784d6ae072a29d68e700c77a49822aa987e422d
sujeong Shin <ssj623862@gmail.com>
2023-08-01 15:40:47 +0900

    second commit

--- js/yunjo.js
+++ js/yunjo.js
@@ -7,12 +7,6 @@ const video02 = document.getElementById('video02');
 const video03 = document.getElementById('video03');
 
 /* 삼항조건연산자 */
-// window.onresize = function(event){
-//     const innerWidth = window.innerWidth;
-//     innerWidth >= "768" ? video01.src = './media/yunjo_video/video_pc/yunjo_video01_pc.mp4' : video01.src = './media/yunjo_video/video_m/yunjo_video01_m.mp4';
-//     innerWidth >= "768" ? video02.src = './media/yunjo_video/video_pc/yunjo_video02_pc.mp4' : video02.src = './media/yunjo_video/video_m/yunjo_video02_m.mp4';
-//     innerWidth >= "768" ? video03.src = './media/yunjo_video/video_pc/yunjo_video03_pc.mp4' : video03.src = './media/yunjo_video/video_m/yunjo_video03_m.mp4';
-// }
 window.onresize = function(event){
     const innerWidth = window.innerWidth;
     innerWidth <= "767" ? video01.src = './media/yunjo_video/video_m/yunjo_video01_m.mp4' : video01.src = './media/yunjo_video/video_pc/yunjo_video01_pc.mp4';
